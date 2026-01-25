c25982d649fcefb527b9ffcb9c88c51c9bb5379e
adishua <adishua@users.noreply.github.com>
2026-01-25 13:20:39 +0200

    reorder

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -31,9 +31,9 @@ export default function Home() {
       
       <main className="flex-grow">
         <Hero />
+        <Services onSelectService={setSelectedServiceId} />
         <HowItWorks />
         <WhyUs />
-        <Services onSelectService={setSelectedServiceId} />
         <About />
         <Testimonials />
         <Contact selectedServiceId={selectedServiceId} />
